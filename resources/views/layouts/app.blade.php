e72a1e541d2811fec21816870ae9951554eaa533
yuki_takahashi <yuk0406@icloud.com>
2021-03-28 13:10:27 +0900

    viewの背景色を変更

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,7 +19,7 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    <link href="{{ asset('css/recipe.css') }}" rel="stylesheet">
+    
 </head>
 <body>
     <div id="app">
